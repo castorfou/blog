dd1422dc4bc01f3d54b86234ba8f9349d4a270d7
castorfou <guillaume.ramelet@gmail.com>
2022-09-23 10:55:13 +0200

    2022-09-23_10:55

---
+++ posts/2022-09-23-inline-images-with-quarto.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+---
+title: "inline images with quarto"
+description: "works for 1st image but not others"
+date: "9/23/2022"
+categories:
+  - nbdev
+  - quarto
+---
+# Here is the 1st image inline
+# 
+# ![image.png](attachment:71a4fc95-5776-4180-ae3a-b815bbf1b7a0.png)
+
+# and here the second one
+# 
+# ![image.png](attachment:74e0d8d8-ad0f-4eca-b52b-167ac518d0e9.png)
+
+# if we see a 1 on this second image, we have a problem
+
+# In[ ]:
+
+
+
+
